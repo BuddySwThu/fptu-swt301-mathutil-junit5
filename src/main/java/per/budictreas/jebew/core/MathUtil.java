0028989222ebe1305edc97546db8348052d369d6
BuddySwThu <ntb1901@gmail.com>
2022-10-20 15:16:00 +0700

    . . .

--- src/main/java/per/budictreas/jebew/core/MathUtil.java
+++ src/main/java/per/budictreas/jebew/core/MathUtil.java
@@ -4,6 +4,7 @@ public class MathUtil {
     //clone bo thu vien Math Util cua JDK
     //gia lap cac ham cua class Math cua JDK
     //public static final double PI = 3.14159265;
+
     public static long getFactorial(int n) {
         if (n < 0 || n > 20) throw new IllegalArgumentException("Invalid number 'n' !!! 'n' must be between 0...20");
         if (n == 0 || n == 1) return 1;
